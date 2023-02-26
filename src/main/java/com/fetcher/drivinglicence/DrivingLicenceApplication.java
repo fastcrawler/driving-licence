5906d2b90d4e4445923a0a98470390db5da7ba64
Ajay <a2singh@ebms.com>
2023-02-26 20:03:17 +0530

    added msg

--- src/main/java/com/fetcher/drivinglicence/DrivingLicenceApplication.java
+++ src/main/java/com/fetcher/drivinglicence/DrivingLicenceApplication.java
@@ -1,13 +1,21 @@
 package com.fetcher.drivinglicence;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DrivingLicenceApplication {
+public class DrivingLicenceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DrivingLicenceApplication.class, args);
+		System.out.println("Application has been started");
+
+		git stat
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Application has been started");
+	}
 }
